3149fa582d0a6650cebc31383c7ce12c4ddded19
macBasement <michaelcoca@gmail.com>
2015-12-10 22:31:42 -0600

    c

---
+++ ch8-ex1/Card.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch8_ex1
+{
+    public class Card
+    {
+        public int Suit { get; set; }
+        public int Value { get; set; }
+        public string Name { get; }
+
+        public Card(Suits suit, Values value)
+        {
+            Name = value.ToString() + " of " + suit.ToString();
+        }
+
+        public enum Suits
+        {
+            Spades,
+            Clubs,
+            Diamonds,
+            Hearts,
+        }
+
+        public enum Values
+        {
+            Ace = 1,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 11,
+            Queen = 12,
+            King = 13,
+        }
+    }
+
+    
+}
--- ch8-ex1/Form1.cs
+++ ch8-ex1/Form1.cs
@@ -15,6 +15,20 @@ namespace ch8_ex1
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int numberBetween0and3 = random.Next(4);
+            int numberBetween1and13 = random.Next(1, 14);
+            int anyRandomInteger = random.Next();
+
+            Card.Suits suit = (Card.Suits)numberBetween0and3;
+            Card.Values value = (Card.Values)numberBetween1and13;
+            Card card = new Card(suit, value);
+            label1.Text = card.Name;
         }
     }
 }
