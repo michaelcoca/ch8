3ed4a7107834673f0d3f8d7147673a7b67f79392
macMain <michaelcoca@gmail.com>
2016-01-31 20:08:44 -0600

    main

---
+++ ch8-duck/Duck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch8_duck
+{
+    class Duck
+    {
+        public int Size;
+        public KindOfDuck Kind;
+
+        
+    }
+
+    enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Decoy,
+    }
+}
